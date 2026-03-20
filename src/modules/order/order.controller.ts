ffa71a606af9d526465513b34b938e47727848c6
Abadi Jerry <abadijerry44@gmail.com>
2026-03-21 00:46:47 +0100

    feat(auth): add JwtAuthGuard to order retrieval endpoints

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Param, Req, Query } from '@nestjs/common';
+import { Controller, Get, Param, Req, Query, UseGuards } from '@nestjs/common';
 import { OrderService } from './order.service';
 import type { Request, Response } from 'express';
 import { GetOrdersQueryDto } from './dto/get-orders-query.dto';
 import { OrderResponseDto, PaginatedOrdersResponseDto } from './interface';
 import { UnauthorizedException } from 'src/common/exceptions';
+import { JwtAuthGuard } from 'src/common/guards/auth.guard';
 
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   async getUserOrders(
     @Req() req: Request,
     @Query() query: GetOrdersQueryDto,
@@ -22,6 +24,7 @@ export class OrderController {
   }
 
   @Get(':orderId')
+  @UseGuards(JwtAuthGuard)
   async getOrderById(
     @Req() req: Request,
     @Param('orderId') orderId: string,
@@ -34,6 +37,7 @@ export class OrderController {
   }
 
   @Get('by-payment/:paymentIntentId')
+  @UseGuards(JwtAuthGuard)
   async getOrderByPaymentIntent(
     @Req() req: Request,
     @Param('paymentIntentId') paymentIntentId: string,
